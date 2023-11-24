7648210210a5065a40aa9aee4b11a7067a57932f
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2023-11-24 17:19:46 +0200

    stream page styles

--- src/components/Stream/Stream.jsx
+++ src/components/Stream/Stream.jsx
@@ -1,49 +1,55 @@
 import { Box } from 'components/Box/Box.styled';
 import ReactPlayer from 'react-player';
 import {
-  HowItWorksSection,
+  ChatBox,
   SectionSubTitle,
   SectionTitle,
   SectionWrapper,
+  StreamBox,
+  StreamSection,
   VideoBox,
-  VideoLimiter
+  VideoLimiter,
 } from './Stream.styled';
 
 export const Stream = () => {
   return (
-    <HowItWorksSection id="howitworks">
+    <StreamSection>
       <Box>
         <SectionWrapper>
           <SectionTitle>
-            HOW IT <SectionSubTitle>WORKS?</SectionSubTitle>
+            УРОК <SectionSubTitle>ONLINE</SectionSubTitle>
           </SectionTitle>
         </SectionWrapper>
-        <VideoLimiter>
-          <VideoBox>
-            <ReactPlayer
-              loop={true}
-              playing={true}
-              muted={true}
-              controls={true}
-              style={{
-                display: 'block',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-              }}
-              width="100%"
-              height="100%"
-              url="https://www.youtube.com/embed/ItvOvNAnk8o"
-            />
-          </VideoBox>
-        </VideoLimiter>
+        <StreamBox>
+          <VideoLimiter>
+            <VideoBox>
+              <ReactPlayer
+                loop={true}
+                playing={true}
+                muted={true}
+                controls={true}
+                style={{
+                  display: 'block',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                }}
+                width="100%"
+                height="100%"
+                url="https://www.youtube.com/embed/ItvOvNAnk8o"
+              />
+            </VideoBox>
+          </VideoLimiter>
+          <ChatBox>
+            <iframe
+              title="chat"
+              width="350px"
+              height="720px"
+              src="https://www.youtube.com/live_chat?v=ItvOvNAnk8o&embed_domain=paulhighrow.github.io"
+            ></iframe>
+          </ChatBox>
+        </StreamBox>
       </Box>
-      <iframe
-      title='chat'
-        width="350px"
-        height="500px"
-        src="https://www.youtube.com/live_chat?v=ItvOvNAnk8o&amp;embed_domain=paulhighrow.github.io"
-      ></iframe>
-    </HowItWorksSection>
+    </StreamSection>
   );
 };
--- src/components/Stream/Stream.styled.js
+++ src/components/Stream/Stream.styled.js
@@ -1,8 +1,6 @@
-import styled, { keyframes } from 'styled-components';
-import { Link } from 'react-scroll';
-import { ReactComponent as MenuArrow } from '../../img/svg/menu-arrow.svg';
+import styled from 'styled-components';
 
-export const HowItWorksSection = styled.section`
+export const StreamSection = styled.section`
   position: relative;
   padding: 30px;
 
@@ -73,116 +71,16 @@ export const SectionSubTitle = styled.span`
   }
 `;
 
-export const PageNavigation = styled.ul`
-  font-size: 26px;
-  font-weight: 600;
-  letter-spacing: 1.8px;
+export const StreamBox = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 9px;
-
-  @media screen and (min-width: 768px) {
-    font-size: 32px;
-    gap: 10px;
-  }
-
-  @media screen and (min-width: 1280px) {
-    font-size: 36px;
-  }
-`;
-
-export const arrowAnimation = keyframes`
-  0%{
-    stroke-dashoffset: -435;
-  }
-  100% {
-    /* closing the offset makes the line appear to be drawn-in */
-    stroke-dashoffset: 0;
-  }
-`;
-
-export const PageNavigationItem = styled.li`
-  z-index: 1;
-`;
-
-export const PageNavigationLink = styled(Link)`
-  position: relative;
-  display: block;
-  z-index: 1;
-  color: var(--main-color);
-  transition: color var(--animation-global), opacity var(--animation-global);
-
-  @media screen and (min-width: 1280px) {
-    cursor: pointer;
-  }
-
-  @media screen and (max-width: 1279px) {
-    user-select: none;
-  }
-
-  &:hover,
-  &:focus {
-    color: var(--accent-color);
-  }
-
-  &:hover > span,
-  &:focus > span {
-    opacity: 1;
-    transition: opacity var(--animation-global) 250ms;
-  }
-
-  &:hover > svg,
-  &:focus > svg {
-    opacity: 1;
-    animation-name: ${arrowAnimation};
-    animation-duration: 400ms;
-    animation-timing-function: linear;
-    animation-iteration-count: 1;
-  }
-`;
-
-export const PageNavigationArrow = styled(MenuArrow)`
-  display: block;
-  opacity: 0;
-  position: absolute;
-  bottom: -5px;
-  left: -5px;
-  width: 250px;
-  z-index: -1;
-  stroke-dasharray: 435;
-
-  @media screen and (min-width: 768px) {
-    bottom: -6px;
-    left: -6px;
-    width: 300px;
-  }
-
-  @media screen and (min-width: 1280px) {
-    bottom: -6px;
-    left: -6px;
-    width: 400px;
-  }
-`;
-
-export const PageNavigationText = styled.span`
-  display: none;
-
-  @media screen and (min-width: 1280px) {
-    display: inline;
-    color: var(--accent-color);
-    opacity: 0;
-    position: absolute;
-    top: 33%;
-    right: -95px;
-    font-size: 22px;
-    font-weight: 400;
-    letter-spacing: normal;
-  }
+  flex-direction: row;
+  gap: 10px;
 `;
 
 export const VideoLimiter = styled.div`
   margin: 0 auto;
   max-width: 640px;
+  width: 100%;
 
   @media screen and (min-width: 768px) {
     max-width: 960px;
@@ -198,4 +96,17 @@ export const VideoBox = styled.div`
   border-radius: 20px;
   overflow: hidden;
   box-shadow: -10px -10px 30px 0px rgba(0, 0, 0, 0.25);
+`;
+
+export const ChatBox = styled.div`
+  position: relative;
+  width: max-content;
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: -10px -10px 30px 0px rgba(0, 0, 0, 0.25);
+
+  & > iframe {
+    border: none;
+    display: block;
+  }
 `;
