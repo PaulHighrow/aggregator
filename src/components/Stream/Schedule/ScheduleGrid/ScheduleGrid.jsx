4360c21d384f1ee3b4683db63ca75f38ba07aa75
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2023-12-12 15:08:46 +0200

    lint fix, stuff deletion, version update

--- src/components/Stream/Schedule/ScheduleGrid/ScheduleGrid.jsx
+++ src/components/Stream/Schedule/ScheduleGrid/ScheduleGrid.jsx
@@ -3,6 +3,7 @@ import "ag-grid-community/styles/ag-grid.css";
 import { useState } from 'react';
 
 export const ScheduleGrid = () => {
+  // eslint-disable-next-line
   const [rowData, setRowData] = useState([
     {
       day: 'monday',
@@ -11,6 +12,7 @@ export const ScheduleGrid = () => {
       polski: '19:00',
     },
   ]);
+  // eslint-disable-next-line
   const [colDefs, setColDefs] = useState([
     { field: 'day' },
     { field: 'english' },
