7e287b159b145b779fc662aa5af72d396fbb9cf3
Kyrylo Sulihan <106202828+PaulHighrow@users.noreply.github.com>
2024-03-18 18:15:59 +0200

    new 3d-viewer

--- src/pages/TeacherPage/Viewer/Viewer.jsx
+++ src/pages/TeacherPage/Viewer/Viewer.jsx
@@ -12,7 +12,7 @@ export const Viewer = ({ isViewerOpen, isOpenedLast, sectionWidth }) => {
   const supportBoxStylesHandler = () => {
     return {
       zIndex: isOpenedLast === 'viewer' ? '4' : '1',
-      width: isFullScreen ? sectionWidth : sectionWidth / 10 * 4,
+      width: isFullScreen ? sectionWidth : (sectionWidth / 10) * 4,
     };
   };
 
@@ -34,12 +34,35 @@ export const Viewer = ({ isViewerOpen, isOpenedLast, sectionWidth }) => {
           )}
         </KahootFullScreenBtn>
         <iframe
-          id="3d-window"
-          title="3d-pin"
-          src="https://3dviewer.net/#"
+          src="https://sketchfab.com/playlists/embed?collection=50a0ac28542842fba993aea82915bfa1&autostart=0"
+          title="CARS 3D MODELS"
+          frameborder="0"
+          allowfullscreen
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+          allow="autoplay; fullscreen; xr-spatial-tracking"
+          xr-spatial-tracking
+          execution-while-out-of-viewport
+          execution-while-not-rendered
+          web-share
           width="100%"
           height="100%"
         ></iframe>
+        {/* <iframe
+          title="19th Century Viennese silver & enamel nef ship"
+          frameborder="0"
+          allowfullscreen
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+          allow="autoplay; fullscreen; xr-spatial-tracking"
+          xr-spatial-tracking
+          execution-while-out-of-viewport
+          execution-while-not-rendered
+          web-share
+          src="https://sketchfab.com/models/878710b7dd6a48998351f3301df7bcab/embed"
+          width="100%"
+          height="100%"
+        ></iframe> */}
       </ViewerBox>
     </>
   );
